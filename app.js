f2efa546983d65c269a55f25aa9aced1f6898968
Maryann Gitonga <34104277+MaryannGitonga@users.noreply.github.com>
2021-11-21 06:28:23 +0300

    Add code to synchronize device twin settings for the device

--- app.js
+++ app.js
@@ -133,9 +133,59 @@ function onSetFanState(request, reponse) {
 
             // Reposrt success back to the hub.
             reponse.send(200, 'Fan state set: ' + request.payload, directMethodResponse);
+            
+            // Confirm changes to the reported properties.
+            sendReportedProperties();
         }
     }
 }
 
 // Set up the handler for the SetFanState direct method call.
 client.onDeviceMethod('SetFanState', onSetFanState);
+
+let deviceTwin;                                         // Global reference to device twin.
+
+// Create a patch to send to the hub.
+const reportedPropertiesPatch = {
+    firmwareVersion: '1.2.3',
+    lastPatchReceivedId: '',
+    fanState: '',
+    currentTemperature: '',
+    currentHumidity: ''
+};
+
+// Send the reported properties patch to the hub.
+function sendReportedProperties() {
+
+    // Prepare the patch.
+    reportedPropertiesPatch.fanState = fanState;
+    reportedPropertiesPatch.currentTemperature = currentTemperature.toFixed(2);
+    reportedPropertiesPatch.currentHumidity = currentHumidity.toFixed(2);
+
+    deviceTwin.properties.reported.update(reportedPropertiesPatch, function (err) {
+        if (err) {
+            redMessage(err.message);
+        } else {
+            greenMessage('\nTwin state reported');
+            greenMessage(JSON.stringify(reportedPropertiesPatch, null, 2));
+        }
+    });
+}
+
+// Handle changes to the device twin properties.
+client.getTwin(function (err, twin) {
+    if (err) {
+        redMessage('could not get twin');
+    } else {
+        deviceTwin = twin;
+        deviceTwin.on('properties.desired', function (v) {
+            desiredTemperature = parseFloat(v.temperature);
+            desiredHumidity = parseFloat(v.humidity);
+            greenMessage('Setting desired temperature to ' + v.temperature);
+            greenMessage('Setting desired humidity to ' + v.humidity);
+
+            // Update the reported properties, after processing the desired properties.
+            sendReportedProperties();
+        });
+    };
+});
